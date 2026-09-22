478c0d36b3c167b6276d3f8133008b33b18f025b
Rita Lopez <rlaphitz@gmail.com>
2026-09-22 17:32:57 -0300

    feat(): agregue un Conductor

--- Ejemplo1.java
+++ Ejemplo1.java
@@ -22,6 +22,7 @@ class TestMovilidadDigital {
     private Conductor conductor2;
     private Conductor conductor3;
     private Conductor conductor4;
+    private Conductor conductor5;
     
     
 
